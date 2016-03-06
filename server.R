6de37c85721271856964fb056bb73622b7cc5df0
guzmanlopez <guzilop@gmail.com>
2016-03-06 18:07:48 -0300

    add example ais data and load it in server.R

--- server.R
+++ server.R
@@ -1,14 +1,15 @@
 library("shiny")
 
 # Load data
-# ais <- readRDS("data/ais.rds")
+# 1 million rows
+ais <- readRDS("data/aisData.rds")
 # vms <- readRDS("data/vms.rds")
 
 shinyServer(function(input, output) {
   
   output$plot <- renderPlot({
     
-    plot <- plot(x = 1:100, y = 1:100, xlab = "Longitud", ylab = "Latitud")
+    plot <- plot(x = ais$LON[1:1000], y = ais$LAT[1:1000], xlab = "Longitud", ylab = "Latitud", pch = 19, col = "black")
     return(plot)
     
   })
