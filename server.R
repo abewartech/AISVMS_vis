3b1f1c82406c4eeeba5e7397236d0b54b2170891
guzmanlopez <guzilop@gmail.com>
2016-03-26 19:41:47 -0300

    not show all vessel names in multiple select

--- server.R
+++ server.R
@@ -1,12 +1,13 @@
 # Packages ----------------------------------------------------------------
 library("shiny")
 library("fasttime")
+library("lubridate")
 
 # Global data -------------------------------------------------------------
 
 ais <- readRDS("data/aisData.rds") # 1 million rows
 colnames(ais)[1] <- c("MMSI")
-ais <- subset(x = ais[1:100000,], select = c("LON", "LAT", "TIMESTAMP", "MMSI"))
+ais <- subset(x = ais[1:500000, ], select = c("LON", "LAT", "TIMESTAMP", "MMSI"))
 ais$TIMESTAMP <- fastPOSIXct(x = ais$TIMESTAMP)
 
 # Initial values
@@ -14,66 +15,77 @@ dateFrom <- min(ais$TIMESTAMP)
 dateUntil <- max(ais$TIMESTAMP)
 dateFromQuery <- dateFrom
 dateUntilQuery <- dateUntil
-vesselNames <- unique(ais$MMSI)
+
+# Not all vessel names
+vesselNames <- unique(ais$MMSI)[1:100]
 
 # vms <- readRDS("data/vms.rds")
 
 # Shiny Server ------------------------------------------------------------
 
 shinyServer(function(input, output) {
-   
-
+  
+  
   # Data --------------------------------------------------------------------
   
   ais_df <- reactive({
     
     # Query time
-    if(input$dateFrom == ""){
+    if(input$dateFrom == "")
+    {
       dateFromQuery <- dateFrom
-    } else {
+    } 
+    else
+    {
       dateFromQuery <- input$dateFrom
     }
-    if(input$dateUntil == ""){
+    if(input$dateUntil == "")
+    {
       dateUntilQuery <- dateUntil
-    } else {
+    }
+    else 
+    {
       dateUntilQuery <- input$dateUntil
     }
     
     ais_df <- subset(x = ais, subset = TIMESTAMP >= dateFromQuery & TIMESTAMP <= dateUntilQuery, select = c("LON", "LAT", "TIMESTAMP", "MMSI"))
     
     # Query vessel name
-    if(input$searchVesselName == "") {
-      
-      if(is.null(input$selectVesselName)) {
+    if(input$searchVesselName == "") 
+    {
+      if(is.null(input$selectVesselName))
+      {
         VesselNameQuery <- vesselNames
-      } else {
+      } 
+      else
+      {
         VesselNameQuery <- input$selectVesselName
       }
-      
-    } else {
+    }
+    else 
+    {
       VesselNameQuery <- input$searchVesselName
     }
     
-    if(length(VesselNameQuery) == 1) {
+    if(length(VesselNameQuery) == 1) 
+    {
       ais_df <- subset(x = ais_df, subset = MMSI == VesselNameQuery, select = c("LON", "LAT", "TIMESTAMP", "MMSI"))
-    } else {
-      
+    } 
+    else 
+    {
       listQueriedVesselsNames <- list()
-      
-      for(i in 1:length(VesselNameQuery)) {
+      for(i in 1:length(VesselNameQuery)) 
+      {
         listQueriedVesselsNames[[i]] <- subset(x = ais_df, subset = MMSI == VesselNameQuery[i], select = c("LON", "LAT", "TIMESTAMP", "MMSI"))
       }
-      
       ais_df <- do.call("rbind", listQueriedVesselsNames)
     }
-    
     return(ais_df)
-    
   })
-
-
+  
+  
   # Point cloud -------------------------------------------------------------
-
+  
   output$plot <- renderPlot({
     
     plot <- plot(x = ais_df$LON[1:10000], y = ais_df$LAT[1:10000], xlab = "Longitud", ylab = "Latitud", pch = 19, col = "black")
@@ -85,49 +97,49 @@ shinyServer(function(input, output) {
   # Mapa
   output$divHtml <- renderUI({
     
-   radius <- input$radius
-   colorGradient <- input$color
-   opacity <- input$opacity
-   blur <- input$blur
-   
-   ais_df <- ais_df()
-   
-   numberOfVessels <- length(unique(ais_df$MMSI))
-
-   # number of rows to toast
-   # Materialize.toast(message, displayLength, className, completeCallback);
-   toast <- paste("Materialize.toast('<i class=material-icons>location_on </i>", nrow(ais_df), " posiciones ', 8000, 'rounded');", sep = "")
-   toast2 <- paste("Materialize.toast('<i class=material-icons>info_outline </i>", numberOfVessels, " barcos ', 9500, 'rounded');", sep = "")
-   
-   j <- paste0("[", ais_df[, "LAT"], ",", ais_df[, "LON"], "]", collapse = ",")
-   j <- paste0("[", j, "]")
-   
-   mapa <- HTML(
-     paste(
-       "<script>",
-       sprintf("var buildingsCoords = %s;", j),
-"buildingsCoords = buildingsCoords.map(function(p) {return [p[0], p[1]];});
+    radius <- input$radius
+    colorGradient <- input$color
+    opacity <- input$opacity
+    blur <- input$blur
+    
+    ais_df <- ais_df()
+    
+    numberOfVessels <- length(unique(ais_df$MMSI))
+    
+    # number of rows to toast
+    # Materialize.toast(message, displayLength, className, completeCallback);
+    toast <- paste("Materialize.toast('<i class=material-icons>location_on </i>", nrow(ais_df), " posiciones ', 8000, 'rounded');", sep = "")
+    toast2 <- paste("Materialize.toast('<i class=material-icons>info_outline </i>", numberOfVessels, " barcos ', 9500, 'rounded');", sep = "")
+    
+    j <- paste0("[", ais_df[, "LAT"], ",", ais_df[, "LON"], "]", collapse = ",")
+    j <- paste0("[", j, "]")
+    
+    mapa <- HTML(
+      paste(
+        "<script>",
+        sprintf("var buildingsCoords = %s;", j),
+        "buildingsCoords = buildingsCoords.map(function(p) {return [p[0], p[1]];});
 if(map.hasLayer(heat)) {map.removeLayer(heat);};
 var heat = L.heatLayer(buildingsCoords, {minOpacity:", opacity,", radius:", radius, colorGradient, ", blur:", blur,"}).addTo(map);",
-toast,
-toast2,
-"</script>"), sep = "")
-   
-   return(mapa)
-   
-   })
+        toast,
+        toast2,
+        "</script>"), sep = "")
+    
+    return(mapa)
+    
+  })
   
   # Table of data -----------------------------------------------------------  
   
   output$table <- renderDataTable({
     ais_df()
-    })
-    
+  })
+  
   
   # Select vessel name ------------------------------------------------------
   
   output$selectVesselName <- renderUI({
-
+    
     # Subset
     ais_df <- ais_df()
     
@@ -141,7 +153,7 @@ toast2,
     # Mark as selected options the previously selected vessels
     
     if(numberOfVessels == length(vesselNamesSelected)) {
-
+      
       options <- list()
       
       for(i in 1:numberOfVessels){
@@ -206,7 +218,7 @@ toast2,
     #"<script>$('#selectVesselName2').material_select();</script>"
     
     return(select)
-
+    
   })
   
   
