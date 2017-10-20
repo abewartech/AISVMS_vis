b1b21d8fe18c2c535b7c6118c100e5ef47540861
Guzmán López <guzilop@gmail.com>
2017-10-20 18:19:36 -0300

    add nvd3 line chart test

--- server.R
+++ server.R
@@ -342,5 +342,16 @@ shinyServer(function(input, output) {
     
   })
   
+  # Line Chart -------------------------------------------
+  
+  output$mychart <- renderLineChart({
+    # Return a data frame. Each column will be a series in the line chart.
+    data.frame(
+      Sine = sin(1:100/10 + input$sinePhase * pi/180) * input$sineAmplitude,
+      Cosine = 0.5 * cos(1:100/10),
+      "Sine 2" = sin(1:100/10) * 0.25 + 0.5
+    )
+  })
+  
 })
 
